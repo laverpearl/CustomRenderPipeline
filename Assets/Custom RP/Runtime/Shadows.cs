45e14b5c2d9cc72d6fe74085e8731a5d5118e3e0
laverpearl <k2811jj@gmail.com>
2024-02-07 00:49:55 +0900

    컬링 바이어스 설정

--- Assets/Custom RP/Runtime/Shadows.cs
+++ Assets/Custom RP/Runtime/Shadows.cs
@@ -175,12 +175,16 @@ public class Shadows
         int tileOffset = index * cascadeCount;
         Vector3 ratios = settings.directional.CascadeRatios;
 
+        float cullingFactor =
+            Mathf.Max(0f, 0.8f - settings.directional.cascadeFade);
+
         for (int i = 0; i < cascadeCount; i++)
         {
             cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(light.visibleLightIndex,
                 i, cascadeCount, ratios, tileSize, light.nearPlaneOffset, out Matrix4x4 viewMatrix, 
                 out Matrix4x4 projectionMatrix, out ShadowSplitData splitData);
 
+            splitData.shadowCascadeBlendCullingFactor = cullingFactor;
             shadowSettings.splitData = splitData;
 
             if (index == 0)
